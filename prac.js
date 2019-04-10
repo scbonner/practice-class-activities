d2afa993d62be67a512d39f301230e1778527ae0
scbonner <codingbootcamp918@gmail.com>
2019-04-10 17:21:51 -0400

    initial push

---
+++ prac.js
@@ -0,0 +1,39 @@
+// in-line comment
+// data types: undefined, null or nothing, boolean true or false, string, symbol, number and object
+// variable store an manipulate data ... a label
+// declare a varible  three ways
+// var is used throughout program
+// let used within the scope where it is declared
+//const is a varible that will never change
+var myName = "Beau";
+
+// assign a varible 
+var a;        
+// (declaring and assiging a varible)
+var b = 2;     
+
+// console.log allow you to see things in the console
+console.log(a)
+
+// initialziing a variable
+var a = 9;
+
+// initialize these three variables (undefined have no value)
+var a;
+var b;
+var c;
+
+// assiging these three variables  value)
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// declarations
+var studyCapVar;
+var properCamelCase;
+var TitleaseOver;
+
+//Assignments
+studyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
